3b48bfc8634fcbe21be00254196fa04dde512404
lizi-oss <2219129948@qq.com>
2026-04-01 09:50:54 +0800

    捣鼓半天发现是没有变成源代码文件我说我不能这么简单都做 错，有点累了。

---
+++ java/hello.java
@@ -0,0 +1,9 @@
+//文件名必须是 hello.java，类名必须和文件名完全一致（大小写一致）
+public class hello {
+    // 这是Java程序的入口，必须有
+    public static void main(String[] args) {
+        // 这里写你要运行的代码
+        System.out.println("Hello, Java!");
+        System.out.println("我的第一个Java程序，成功运行啦！");
+    }
+}
