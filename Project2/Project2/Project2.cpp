828bb68f9e60f32bc0d17e0f8eff52b0bcd07425
lizi-oss <2219129948@qq.com>
2026-04-02 21:23:06 +0800

    Hello World

---
+++ Project2/Project2/Project2.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+int main() {
+    int arr[10] = {};
+    for (int i = 0;i < 10;i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    int h;
+    scanf("%d", &h);
+    int count = 0;
+    for (int i = 0;i < 10;i++) 
+    {
+        if (arr[i] <= h + 30) 
+        {
+            count++;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
