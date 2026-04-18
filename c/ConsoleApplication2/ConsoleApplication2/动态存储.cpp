582ffa73205c07864e51c1b77dd7fec856909c30
lizi-oss <2219129948@qq.com>
2026-04-18 13:50:27 +0800

    存储静态存储动态存储

---
+++ c/ConsoleApplication2/ConsoleApplication2/动态存储.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+// 动态内存版：用 new 分配堆内存
+int* getMax(int arr[], int len) {
+    // 先找最大值
+    int max_val = arr[0];
+    for (int i = 1; i < len; i++) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+    }
+
+    // 在堆上分配一块 int 空间，存最大值
+    int* p_max = new int;
+    *p_max = max_val;
+
+    // 返回这块堆内存的地址
+    return p_max;
+}
+
+int main() {
+    int arr[] = { 10, 30, 20, 50, 40 };
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    int* result = getMax(arr, len);
+    cout << "最大值是：" << *result << endl;
+
+    // 关键！用完必须 delete，释放堆内存，避免内存泄漏
+    delete result;
+
+    return 0;
+}
---
+++ c/ConsoleApplication2/ConsoleApplication2/静态存储.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+// static 版：用静态变量存结果
+int* getMax(int arr[], int len) {
+    // static 变量：全局只有一份，程序结束才销毁
+    static int max_val;
+    max_val = arr[0];
+
+    for (int i = 1; i < len; i++) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+    }
+
+    // 返回 static 变量的地址
+    return &max_val;
+}
+
+int main() {
+    int arr[] = { 10, 30, 20, 50, 40 };
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    int* result = getMax(arr, len);
+    cout << "最大值是：" << *result << endl;
+
+    // 注意：static 变量不需要手动 delete，程序结束自动释放
+
+    return 0;
+}
