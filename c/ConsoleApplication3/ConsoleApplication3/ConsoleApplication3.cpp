bcb5e8b635a4775d0a247c0005ab1af436b848e4
lizi-oss <2219129948@qq.com>
2026-04-25 16:19:32 +0800

    函数的分文件使用
    For the multi-file usage of functions:first,create a header file with the suffix.h.Write the code #include<iostream>and using namespace std；in it,followed by function declarations.Next ,create a source  file with the suffix.cpp.At the start of this file ,write #include "filename.h",then define the specific implementation of the functions.When you need to call these custom functions in other files,simply add #include"filename.h"to directly use them.

---
+++ c/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+#include"Swap.h"
+
+int main()
+{
+    int a = 100, b = 520;
+    cout << "交换前a=" << a << "b=" << b << endl;
+    swap(&a, &b);
+    cout << "交换后a=" << a << "b=" << b << endl;
+    std::cout << "Hello World!\n";
+}
+
