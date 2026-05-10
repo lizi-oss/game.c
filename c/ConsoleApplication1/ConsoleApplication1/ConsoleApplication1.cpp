28fdbc3a099d53a69cbc48a2ff32522276444241
lizi-oss <2219129948@qq.com>
2026-05-10 22:40:59 +0800

    new delete and class
    new/delete 的使用，还有创建类

---
+++ c/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+using namespace std;
+
+//单个 new → 对应 delete
+//数组 new[] → 对应 delete[]
+//指针访问成员用->，普通对象用 .
+//new 必须手动 delete 释放内存
+int main() {
+	//new基础单个变量
+	//堆上开辟int
+	int* p = new int;
+	*p = 100;
+	cout << *p << endl;
+	//释放
+	delete p;
+	p = nullptr;
+
+	//new 变量并直接初始化
+	int* p2 = new int(200);
+	cout << *p2 << endl;
+	delete p2;
+	p2 = nullptr;
+
+	//new 数组+delete[]
+	int* arr = new int[5];
+	for (int i = 0; i < 5; i++) {
+		arr[i] = i + 1;
+		cout << arr[i] << endl;
+	}
+	//数组必须delete[]
+	delete[] arr;
+	arr = nullptr;
+	
+	//new
+
+	return 0;
+}
---
+++ c/ConsoleApplication18/ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+const double PI = 3.14;
+
+class Circle {
+public:
+	//在这里写这个类的属性
+	//半径
+	int m_r;
+	//行为，通俗一点就是写函数写明白这个封装到底要干嘛，就是和Java有一点点区别吧。
+	//获取圆的周长
+	double calculateZC()
+	{
+		return 2* PI* m_r;
+	}
+};
+
+int main() {
+
+	Circle c1;//调用时和JAVA差不多都是利用这里去进行类的调用。
+	c1.m_r = 10;//给类中的属性下定义
+	cout << "圆的周长为：" << c1.calculateZC()<< endl;//这里调用时
+	
+	//new创建类对象
+	Circle* c2 = new Circle;
+	c2->m_r = 20;//指针用->
+	cout << c2->calculateZC() << endl;
+	delete c2;
+	c2 = nullptr;
+
+	//new 数组对象
+	Circle* arr = new Circle[3];
+	arr[0].m_r = 5;
+	cout << arr[0].calculateZC() << endl;
+	//数组对象必须delete[]
+	delete[]arr;
+	arr = nullptr;
+	 
+	return 0;
+}
