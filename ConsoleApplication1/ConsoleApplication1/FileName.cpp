6a2f08adfeb4eb7a6c1e556f5eea83eb05a51e99
lizi-oss <2219129948@qq.com>
2026-05-10 23:09:01 +0800

    c++封装的学习
    理解c++封装的底层逻辑，区分和Java的区别。不要学混了

---
+++ ConsoleApplication1/ConsoleApplication1/FileName.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+class Student {
+public:
+	string Name;
+	long long ID;
+	void setName() {
+		cout << "Enter name: " << endl;
+		cin >> Name;
+	}
+	void setID() {
+		cout << "Enter ID: " << endl;
+		cin >> ID;
+	}
+	void info() {
+				cout << "Name: " << Name << endl;
+				cout << "ID: " << ID << endl;
+	}
+};
+
+int main() {
+	Student s1;
+	s1.setName();
+	s1.setID();
+	s1.info();
+	return 0;
+}
